682fa1170ecf88b2ce2f947192eb6f14e5baefde
favourflav <chukwagon1@gmail.com>
2024-04-17 02:24:43 -0700

    fixed small bug with car input schema price

--- client/src/components/multiStepDivs/carDivs/CarComponets/carModalSchema.ts
+++ client/src/components/multiStepDivs/carDivs/CarComponets/carModalSchema.ts
@@ -1,47 +1,46 @@
 import { z } from "zod";
 
 export const carsArr = [
-    "Acura",
-    "AlfaRomeo",
-    "Audi",
-    "BMW",
-    "Buick",
-    "Cadillac",
-    "Chevrolet",
-    "Chrysler",
-    "Dodge",
-    "FIAT",
-    "Ford",
-    "Genesis",
-    "GMC",
-    "Honda",
-    "Hyundai",
-    "INFINITI",
-    "Jaguar",
-    "Jeep",
-    "Kia",
-    "LandRover",
-    "Lexus",
-    "Lincoln",
-    "Lucid",
-    "Maserati",
-    "Mazada",
-    "MercedesBenz",
-    "MINI",
-    "Mitsubishi",
-    "Nissan",
-    "Polestar",
-    "Porsche",
-    "Ram",
-    "Rivian",
-    "Scion",
-    "Subaru",
-    "Telsa",
-    "Toyota",
-    "Volkswagen",
-    "Volvo",
-  ];
-
+  "Acura",
+  "AlfaRomeo",
+  "Audi",
+  "BMW",
+  "Buick",
+  "Cadillac",
+  "Chevrolet",
+  "Chrysler",
+  "Dodge",
+  "FIAT",
+  "Ford",
+  "Genesis",
+  "GMC",
+  "Honda",
+  "Hyundai",
+  "INFINITI",
+  "Jaguar",
+  "Jeep",
+  "Kia",
+  "LandRover",
+  "Lexus",
+  "Lincoln",
+  "Lucid",
+  "Maserati",
+  "Mazada",
+  "MercedesBenz",
+  "MINI",
+  "Mitsubishi",
+  "Nissan",
+  "Polestar",
+  "Porsche",
+  "Ram",
+  "Rivian",
+  "Scion",
+  "Subaru",
+  "Telsa",
+  "Toyota",
+  "Volkswagen",
+  "Volvo",
+];
 
 export const carModalSchema = z
   .object({
@@ -65,9 +64,16 @@ export const carModalSchema = z
       .min(1, {
         message: "Min length is 1",
       }),
-    price: z.string({
-      required_error: "Please enter a price",
-    }),
+    price: z
+      .string({
+        required_error: "Please enter a price",
+      })
+      .refine((item) => parseInt(item) >= 1000, {
+        message: "Please enter a price greater than or equal to $1,000",
+      })
+      .refine((item) => parseInt(item) <= 750000, {
+        message: "Please enter a price less than or equal to $750,000",
+      }),
     mileage: z.string({
       required_error: "Please enter a mileage",
     }),
@@ -88,15 +94,24 @@ export const carModalSchema = z
       .refine((item) => item.length > 0, {
         message: "Please enter a number between 0% and 39%",
       }),
-    salary: z.string({
-      required_error: "Please enter your salary",
-    }),
-    term: z.number({
-      required_error: "Please select a time",
-    }),
+    salary: z
+      .string({
+        required_error: "Please enter your salary",
+      })
+      .refine((item) => parseInt(item) <= 1000000, {
+        message: "Please enter an income/salary less than 1,000,000",
+      }),
+      term: z.number({
+        required_error: "Please select a time",
+      }).refine((item) => item <= 120 , {
+        message: "Please enter a number less than or equal 120 months",
+      })
+      .refine((item) => item >= 36 , {
+        message: "Please enter a number greater than or equal 36 months",
+      }),
     id: z.string(),
     extraPayment: z.string(),
-    img: z.any().optional()
+    img: z.any().optional(),
   })
   .superRefine((values, ctx) => {
     if (parseInt(values.downPayment) >= parseInt(values.price)) {
