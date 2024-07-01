7a7ceb3727cb8e883d9c8d25b968a2434a77ce64
unknown <chukwagon1@gmail.com>
2024-07-01 02:05:09 -0700

    Fix: On mac book modal was to long height ... add max-h-[95%] to make sure the height of modal is not too big

--- client/src/pages/HowToPage/components/BookModal/BookModal.tsx
+++ client/src/pages/HowToPage/components/BookModal/BookModal.tsx
@@ -18,7 +18,7 @@ export default function BookModal({ open, handleClose, handleOpen, item }: IBook
 
   return (
     <Modal open={open} onClose={handleClose}>
-      <div className=" absolute top-[50%] text-lightText dark:text-darkText left-[50%] transfrom -translate-x-[50%] -translate-y-[50%]  dark:bg-homeBg bg-lightHomeBg sm:w-[450px] w-full sm:h-auto h-full  sm:rounded-lg border-[3px] border-gray-400 overflow-y-auto ">
+      <div className=" absolute top-[50%] text-lightText dark:text-darkText left-[50%] transfrom -translate-x-[50%] -translate-y-[50%]  dark:bg-homeBg bg-lightHomeBg sm:w-[450px] w-full sm:max-h-[95%] h-full  sm:rounded-lg border-[3px] border-gray-400 overflow-y-auto ">
         {/* Content */}
         <div className="w-full flex flex-col sm:py-4 px-4 py-2 ">
           {/* Clear Button */}
